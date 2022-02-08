9489ce953c135317b7fdd931ae8aa7c742e640d0
attila kekesi <kekesi.att@gmail.com>
2022-02-09 00:07:25 +0100

    added test to practicepython_05.py

--- practicepython_05.py
+++ practicepython_05.py
@@ -9,6 +9,7 @@
 #   2. Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 
 import random
+import numpy as np
 
 
 def func_common(list_1: list, list_2: list) -> list:
@@ -29,8 +30,10 @@ if __name__ == "__main__":
     list_1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
     list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     common = func_common(list_1, list_2)
-    print(f"list_1: {list_1}\nlist_2: {list_2}")
-    print(f"common: {common}")
+    common_check = np.intersect1d(list_1, list_2)
+    if set(common) == set(common_check):
+        print(f"list_1: {list_1}\nlist_2: {list_2}")
+        print(f"common: {common}")
     print("----------------------------------------------------")
     number_min = 0
     number_max = 13
@@ -39,5 +42,7 @@ if __name__ == "__main__":
     list_1 = [random.randint(number_min, number_max) for _ in range(random.randint(list_min, list_max))]
     list_2 = [random.randint(number_min, number_max) for _ in range(random.randint(list_min, list_max))]
     common = func_common(list_1, list_2)
-    print(f"list_1: {list_1}\nlist_2: {list_2}")
-    print(f"common: {common}")
+    common_check = np.intersect1d(list_1, list_2)
+    if set(common) == set(common_check):
+        print(f"list_1: {list_1}\nlist_2: {list_2}")
+        print(f"common: {common}")
