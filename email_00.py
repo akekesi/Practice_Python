9ce21947e45947a4bb6e4cdd8fac55129df629fb
attila kekesi <kekesi.att@gmail.com>
2022-03-12 22:00:17 +0100

    refactored email_00.py

--- email_00.py
+++ email_00.py
@@ -4,17 +4,27 @@ import smtplib
 import imghdr
 from email.message import EmailMessage
 
+
 email_address = "kekesi.att@gmail.com"
 email_password = ""
 email_to = ["kekesi.att@gmail.com", "kekkencs@gmail.com", "kekesati@schaeffler.com"]
+files_image = ["email_image1.jpg", "email_image2.jpg"]
+files_pdf = ["email_protocol.pdf"]
+
 msg = EmailMessage()
 msg["Subject"] = "Test Email"
 msg["From"] = email_address
 msg["To"] = ", ".join(email_to)
 msg.set_content("Hi,\n\nThis is a test mail sent by python")
 
-files_image = ["image1.jpg", "image2.jpg"]
-files_pdf = ["protocol.pdf"]
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
+    </body>
+</html>
+""", subtype="html")
 
 for file in files_image:
     with open(file, "rb") as f:
@@ -29,15 +39,6 @@ for file in files_pdf:
         file_name = f.name
     msg.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
 
-# msg.add_alternative("""\
-# <!DOCTYPE html>
-# <html>
-#     <body>
-#         <h1 style="color:SlateGray;">This is an HTML Email!</h1>
-#     </body>
-# </html>
-# """, subtype='html')
-
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login(input(email_address, email_password))
+    smtp.login(email_address, email_password)
     smtp.send_message(msg)
